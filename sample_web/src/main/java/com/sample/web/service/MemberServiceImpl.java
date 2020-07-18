c7d678eb726d6acbb3de485bc5547a202af932af
wanni0928 <50094651+wanni0928@users.noreply.github.com>
2020-07-18 17:33:55 +0900

    comment_auth

---
+++ sample_web/src/main/java/com/sample/web/service/MemberServiceImpl.java
@@ -0,0 +1,44 @@
+package com.sample.web.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.sample.web.dao.MemberDAO;
+import com.sample.web.vo.MemberVO;
+
+@Service
+public class MemberServiceImpl implements MemberService {
+
+	@Inject
+	MemberDAO dao;
+
+	@Override
+	public void register(MemberVO vo) throws Exception {
+
+		dao.register(vo);
+
+	}
+
+	@Override
+	public MemberVO login(MemberVO vo) throws Exception {
+		return dao.login(vo);
+	}
+
+	// Controller에서 보내는 파라미터들을 memberUpdate(MemberVO vo)로 받고
+	@Override
+	public void memberUpdate(MemberVO vo) throws Exception {
+		// 받은 vo를 DAO로 보내줍니다.
+		dao.memberUpdate(vo);
+
+	}
+
+	// 업데이트에서 처리한 내용과 같습니다.
+	@Override
+	public void memberDelete(MemberVO vo) throws Exception {
+		dao.memberDelete(vo);
+	}
+
+}
