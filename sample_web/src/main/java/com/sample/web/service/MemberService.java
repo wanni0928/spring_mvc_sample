ba191384dc77dd9cfa24fe889374f6cca47e4163
wanni0928 <50094651+wanni0928@users.noreply.github.com>
2020-07-20 16:31:27 +0900

    spring security and auth id,pass check

--- sample_web/src/main/java/com/sample/web/service/MemberService.java
+++ sample_web/src/main/java/com/sample/web/service/MemberService.java
@@ -9,4 +9,7 @@ public interface MemberService {
 	public MemberVO login(MemberVO vo) throws Exception;
 	public void memberUpdate(MemberVO vo) throws Exception;
 	public void memberDelete(MemberVO vo) throws Exception;
+	public int passChk(MemberVO vo) throws Exception;
+	public int idChk(MemberVO vo) throws Exception;
+
 }
